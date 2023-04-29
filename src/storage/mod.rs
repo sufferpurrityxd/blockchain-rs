a5ac00e976d783b06c58490b169f253692d17664
sufferpurrityxd <11111lesha714@gmail.com>
2023-04-29 12:09:53 +0300

    storage>get_block() function

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -4,18 +4,20 @@ use leveldb::{
   error::Error,
   options::Options,
   database::Database,
-  options::WriteOptions,
+  options::{
+    WriteOptions,
+    ReadOptions,
+  },
 };
-use leveldb::options::ReadOptions;
 use serde::{
   Serialize,
   Deserialize,
 };
+use crate::chain::block::Block;
 
 pub struct Storage(pub Database<i32>);
 
 impl Storage {
-
   pub fn new() -> Storage {
     let path = std::env::var("HOME")
       .map(|home| Path::new(&home).join(".blockchain-rs"))
@@ -32,7 +34,7 @@ impl Storage {
       Database::open(path.as_path(), db_options)
         .map_err(|e| {
           log::error!("Get error when trying to open leveldb: {e:?}");
-          std::process::exit(1)
+          std::process::exit(1);
         })
         .unwrap(),
     )
@@ -41,7 +43,7 @@ impl Storage {
   pub fn add_item<'a, Item: Serialize + Deserialize<'a>>(
     &self,
     key: i32,
-    item: Item
+    item: Item,
   ) -> Result<(), Error> {
     return self
       .0
@@ -54,4 +56,18 @@ impl Storage {
       );
   }
 
+  pub fn get_block(
+    &self,
+    key: i32,
+  ) -> Option<Result<Block, Box<bincode::ErrorKind>>> {
+    return match self.0.get(ReadOptions::new(), key) {
+      Ok(item) => {
+        match item {
+          Some(bytes) => Some(bincode::deserialize::<Block>(bytes.as_slice())),
+          None => None,
+        }
+      },
+      Err(_) => None,
+    }
+  }
 }
